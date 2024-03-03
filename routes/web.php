6b9465675d9cf460645ab3021d7eaae376984e01
Ana Bellatus <anabellatus@gmail.com>
2024-03-03 10:33:45 +0700

    add resources controller

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\PhotoController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -59,8 +60,18 @@ Route::get('/user/profile', function () {
 
 Route::get('/hello', [WelcomeController::class, 'hello']);
 
-Route::get('/', [HomeController::class, 'index']);
+Route::get('/', HomeController::class);
 
-Route::get('/about', [AboutController::class, 'about']);
+Route::get('/about', AboutController::class);
 
-Route::get('/articles/{id}', [ArticleController::class, 'articles']);
+Route::get('/articles/{id}', ArticleController::class);
+
+// Route::resource('photos', PhotoController::class);
+
+Route::resource('photos', PhotoController::class)->only([
+    'index', 'show'
+]);
+
+Route::resource('photos', PhotoController::class)->except([
+    'create', 'store', 'update', 'destroy'
+]); 
